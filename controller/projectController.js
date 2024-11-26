0930db754836a8a71bc8711a7d2666663b78016f
sandy <sandeshjadhav2002@gmail.com>
2024-11-27 02:28:24 +0530

    Feat : signup user done

---
+++ controller/projectController.js
@@ -0,0 +1,6 @@
+const createProject = async(req,res) => {
+   const {projectName, apiKeys} = req.body;
+   
+}
+
+export{createProject}
--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,39 @@
-const signupUser = (req,res) =>{
-     res.send("Hi im from signup")
-}
+import User from "../models/userModel.js";
+import genrateTokenAndSetCookie from "../utils/helpers/genrateTokenAndSetCookie.js";
 
+const signupUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const user = await User.findOne({ email });
 
-export {signupUser}
+    if (user) {
+      return res
+        .status(400)
+        .json({ error: "User already exists try to log in" });
+    }
+
+    const newUser = new User({
+      username,
+      email,
+      password,
+    });
+
+    await newUser.save();
+
+    if (newUser) {
+      genrateTokenAndSetCookie(newUser._id, res);
+      res.status(201).json({
+        _id: newUser._id,
+        name: newUser.username,
+        email: newUser.email,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user Data" });
+    }
+  } catch (err) {
+    res.status(500);
+    console.log("Error in signup user ", err);
+  }
+};
+
+export { signupUser };
