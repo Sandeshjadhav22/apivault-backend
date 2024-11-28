e19e235df29b363fff2ec456eb3a071ede927266
sandy <sandeshjadhav2002@gmail.com>
2024-11-29 02:12:56 +0530

    sort while fetching all project

--- controller/projectController.js
+++ controller/projectController.js
@@ -80,7 +80,7 @@ const getAllProjects = async (req, res) => {
   try {
     const userId = req.user._id;
 
-    const projects = await Project.find({ user: userId });
+    const projects = await Project.find({ user: userId }).sort({createdAt:-1});
     if (!projects || projects.length === 0) {
       return res.status(404).json({ message: "No Project found" });
     }
