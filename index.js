b4a9a0d4774c78b4b3f930c35d21cdc4e786e391
sandy <sandeshjadhav2002@gmail.com>
2024-11-30 19:18:19 +0530

    vercel mod

--- index.js
+++ index.js
@@ -3,38 +3,32 @@ import connectDB from "./mongoDB/connect.js";
 import cookieParser from "cookie-parser";
 import * as dotenv from "dotenv";
 
-import userRoutes from "./routes/userRoutes.js"
-import projectRoutes from "./routes/projectRoutes.js"
+import userRoutes from "./routes/userRoutes.js";
+import projectRoutes from "./routes/projectRoutes.js";
 
 dotenv.config();
-connectDB(process.env.MONGODB_URL);
 
-const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello sandyy");
 });
 
-
 //*Middlewares
-app.use(express.json()); //to parse JSON data in req.body
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
 //*Routes
-app.use('/api/users',userRoutes);
-app.use('/api/projects',projectRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/projects", projectRoutes);
 
+export default app;
 
-const startServer = () => {
-  try {
-    app.listen(PORT, () => {
-      console.log(`Server started on  PORT : ${PORT}`);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
-startServer();
+if (process.env.NODE_ENV !== "vercel") {
+  connectDB(process.env.MONGODB_URL);
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server started on PORT: ${PORT}`);
+  });
+}
