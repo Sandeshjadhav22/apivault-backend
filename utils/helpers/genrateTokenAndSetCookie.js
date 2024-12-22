2ffcb4d93cc6896d3f74db6cd1b5f599243dee5f
sandy <sandeshjadhav2002@gmail.com>
2024-12-22 23:19:49 +0530

    token fix

--- utils/helpers/genrateTokenAndSetCookie.js
+++ utils/helpers/genrateTokenAndSetCookie.js
@@ -4,18 +4,13 @@ const genrateTokenAndSetCookie = (userId,res) => {
    const token = jwt.sign({userId},process.env.JWT_SECRET,{
     expiresIn: '15d',
    })
+   localStorage.setItem("jwt",token)
    res.cookie("jwt",token,{
     httpOnly: true,
     maxAge: 15 * 24 * 60 * 60 * 1000,
     sameSite:"none",
    })
-   res.setHeader('Content-Security-Policy', "script-src 'self'");
-   res.send(`
-      <script>
-         localStorage.setItem('token', '${token}');
-         window.location.href = '/';
-      </script>
-   `);
+ 
 
    return token
 }
